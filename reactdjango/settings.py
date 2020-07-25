0ff1aadd318c21c5f33b82e5039d076ecafb38b2
amitkhetan <amit@techis.io>
2020-07-25 06:30:48 +0530

    debug mode turned on for prod

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '463hzk*onkb4h&+j@mof$wc(20t$$l17w#(v8-6ipzq7m=$a=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['luciapp.herokuapp.com', '127.0.0.1', 'luci.50gram.ingit ']
 
